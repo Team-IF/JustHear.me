b1abc963e350a5378be0c665d9140a8f911d90fe
doohee <doohee006@gmail.com>
2019-08-04 00:02:17 +0900

    Add database connection

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import pymysql
+import uuid
 
 app = Flask(__name__)
-
+db = "나도 몰라 이새끼야 "
+cursor = db.cursor()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
+    return f'데이터베이스 버전은 {data}입니다. 생성된 uuid 는 {uuid.uuid4()}입니다'
 
 
 if __name__ == '__main__':
