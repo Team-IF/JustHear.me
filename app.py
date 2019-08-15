f90fd721507c6c7ef98d8f636c1102023b9e65ac
BuildTools <unconfigured@null.spigotmc.org>
2019-08-16 01:55:34 +0900

    Edit app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask
+#!/usr/bin/env pypy3
+from flask import Flask, Response
 import pymysql
 import uuid
+import json
+import datetime
 
+context = ("/etc/letsencrypt/live/nanobot.tk/fullchain.pem","/etc/letsencrypt/live/nanobot.tk/privkey.pem")
 app = Flask(__name__)
-db = "나도 몰라 이새끼야"
-cursor = db.cursor()
+db = pymysql.connect(unix_socket="/var/run/mysqld/mysqld.sock",user="hearme",password="dhdh4321",db="hearme",charset="utf8")
+cursor = db.cursor(pymysql.cursors.DictCursor)
 
 
-@app.route('/', methods=['get'])
-def hello_world():
-    cursor.execute("SELECT VERSION()")
-    data = cursor.fetchone()
-    return f'데이터베이스 버전은 {data}입니다. 생성된 uuid 는 {uuid.uuid4()}입니다'
+@app.route('/profile/<uuid>', methods=['get'])
+def profile_get(uuid):
+    cursor.execute(f"SELECT * from user_data where uuid='{uuid}'")
+    data = cursor.fetchall()
+    data = data[0]
+    data['birthday'] = data['birthday'].strftime('%Y%m%d')
+    return Response(json.dumps(data), mimetype='application/json; charset=utf-8')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port=6000,ssl_context=context)
