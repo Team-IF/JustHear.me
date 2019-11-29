18c4124c28dca98f14fcfc765165ac65d7eabb8d
Socio-path <doohee006@gmail.com>
2019-11-29 21:17:27 +0900

    Edited typo

--- api/auth.py
+++ api/auth.py
@@ -18,17 +18,17 @@ emailregex = re.compile('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$')
 def login():
     try:
         if not request.is_json:
-            return app.rerror("invalid json", 400)
+            return common.rerror("invalid json", 400)
 
         req = request.json
         if not req.get('email') or not req.get("pw"):
-            return app.rerror("이메일과 비밀번호를 입력해 주세요.", 400)
+            return common.rerror("이메일과 비밀번호를 입력해 주세요.", 400)
 
         app.cursor.execute("SELECT * from user_data where email=%s", request.json.get('email'))
         fetchs = app.cursor.fetchall()
 
         if not fetchs or len(fetchs) == 0:
-            return app.rerror("잘못된 이메일/비밀번호", 403)
+            return common.rerror("잘못된 이메일/비밀번호", 403)
 
         data = fetchs[0]
         if bcrypt.checkpw(request.json.get('pass').encode('utf-8'), data.get('pass').encode('utf-8')):
@@ -42,20 +42,20 @@ def login():
                 'uuid': data['uuid']
             }))
         else:
-            return app.rerror("잘못된 이메일/비밀번호", 403)
+            return common.rerror("잘못된 이메일/비밀번호", 403)
 
     except Exception as e:
-        return app.rerror(e, 500)
+        return common.rerror(e, 500)
 
 @auth.route('/register', methods=['POST'])
 def register():
     try:
         if not request.is_json:
-            return app.rerror("invalid json", 400)
+            return common.rerror("invalid json", 400)
 
         req = request.json
         if not emailregex.search(req.get('email')):
-            return app.rerror("invalid email",400)
+            return common.rerror("invalid email",400)
 
         uuid = str(uuid4())
         username = req.get('username')
@@ -68,11 +68,11 @@ def register():
         try:
             app.cursor.execute("INSERT INTO `hearme`.`user_data` (`uuid`, `username`, `email`, `pass`, `phonenumber`) VALUES (%s,%s,%s,%s,%s) ", values)
         except Exception as e:
-            return app.rerror(e, 500)
+            return common.rerror(e, 500)
             #선택적 정보 넣는건 일단 이거 돌아 가긴 하는지 보고 수정할께요 엉엉엉 테스트를 못하겠어
 
     except Exception as e:
-        return app.rerror(e, 500)
+        return common.rerror(e, 500)
 
 # delete login token
 @auth.route('/login', methods=['DELETE'])
@@ -81,14 +81,14 @@ def invalidate_token():
         token = request.headers['x_access_token']
 
         if not token:
-            return app.rerror("로그인을 해주세요.", 403)
+            return common.rerror("로그인을 해주세요.", 403)
 
         app.cursor.execute("DELETE FROM sessions WHERE accessToken=%s", token)
         app.db.commit()
         return Response(status=204)
 
     except Exception as e:
-        return app.rerror(e)
+        return common.rerror(e)
 
 
 # refresh login token
