b53d12283178a3634ae563f85a19698bc2426ca4
Coder Iro <doohee006@gmail.com>
2020-04-26 15:36:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const router = require('./router/main')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
