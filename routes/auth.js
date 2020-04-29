55b8e13e946f16d73e092b614a189994a47325b6
AlphaBs <ksi123456ab@naver.com>
2020-04-29 18:32:25 +0900

    add auth.js

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const uuid4 = require('uuid4');
+const router = express.Router();
+let db;
+
+router.use(express.json());
+
+router.post('/login', (req, res) => {
+    if (!req.body.email || !req.body.pass)
+        throw new HttpError(400, "이메일과 비밀번호를 입력해 주세요.");
+
+    let user = User.fromEmail(req.body.email);
+    if (!user)
+        throw new HttpError(403, "잘못된 이메일/비밀번호");
+
+    let pw = encPw(req.body.pass);
+    if (user.pass !== pw)
+        throw new HttpError(403, "잘못된 이메일/비밀번호");
+});
+
+
+module.exports = function (s) {
+    db = s;
+    return router;
+};
