0dfe285eab6b0a21ece178131a3b7937cf69d65a
AlphaBs <ksi123456ab@naver.com>
2020-05-14 19:57:00 +0900

    verify query result in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,11 @@
-const express = require("express");
 const Session = require('../models/session');
 
 const accessTokenHeader = 'x-access-token';
 
-module.exports = async function (req, res) {
+module.exports = async function (req, res, next) {
     const token = req.get(accessTokenHeader);
     const session = await Session.findByToken(token);
-
-    if (session)
-        req.session = new Session(session.uuid, session.accessToken, session.expireDate);
+    if (session && !session.errors)
+        req.session = session;
+    next();
 };
