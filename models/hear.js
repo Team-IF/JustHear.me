aa805239dbbc737a943142c43e114f0d1a585b9e
AlphaBs <ksi123456ab@naver.com>
2020-05-15 14:29:59 +0900

    add required limitation in hear model

--- models/hear.js
+++ models/hear.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const hearSchema = new Schema({
-    userId: String,
-    title: String,
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
     content: String,
     createdAt: Date
 }, {
